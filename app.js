cac53edb76f6b37858927c54835c6974dd293b42
Zeng <zeng@Zengs-Mac-mini.lan>
2020-05-16 22:32:05 +0800

    fix

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ App({
     let app = this;
     console.log('getXConfig');
     wx.request({
-      url: 'https://gong.fengniaotuangou.cn/api/user/config?school=all&version=1992',
+      url: 'https://gong.fengniaotuangou.cn/api/user/config?school=all&version=1993',
       method:'GET',
       success:function(res){
         console.log(res.data.data);
