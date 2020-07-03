672f399b07ed167cefc76664a8dbb437150e1360
soleil <a617759082@126.com>
2020-07-03 17:46:06 +0800

    gengxin

--- pages/personal/class-management/manage/manage.js
+++ pages/personal/class-management/manage/manage.js
@@ -19,14 +19,14 @@ Page({
       title: '加载中',
     })
     wx.request({
-      url: app.globalData.host + '/class/students?token=' + wx.getStorageSync('token') + '&page=' + 1 + '&limit=' + 4,
+      url: app.globalData.host + '/class/students?token=' + wx.getStorageSync('token'),
       method: 'get',
       success: function (res) {
         console.log('班级学生在校列表')
         console.log(res)
-        if (res.data.data.data.data) {
+        if (res.data.data) {
           that.setData({
-            childrenList: res.data.data.data.data
+            childrenList: res.data.data
           })
         }
       }
